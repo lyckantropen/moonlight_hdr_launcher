ef09459808852bb047cf83b62dd2ee1d616701f7
Karol Trojanowski <trojanowski.ifuj@gmail.com>
2022-03-13 15:45:20 +0100

    Update C++ formatting

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 #pragma warning(disable : 4244)
 #include <boost/process.hpp>
 #pragma warning(pop)
+#include "windows.h"
 #include <boost/property_tree/ini_parser.hpp>
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <chrono>
-#include <sstream>
 #include <optional>
-#include "windows.h"
-
+#include <sstream>
+#include <string>
 
 #include "WinReg.hpp"
 #include "hdr_toggle.hpp"
@@ -33,140 +32,128 @@ using namespace std::string_literals;
 const static std::string default_launcher = "C:/Program Files (x86)/GOG Galaxy/GalaxyClient.exe";
 
 DEVMODE const get_primary_display_registry_settings() {
-    DEVMODE devmode{};
-    devmode.dmSize = sizeof(devmode);
-    auto rval = EnumDisplaySettings(nullptr, ENUM_REGISTRY_SETTINGS,&devmode);
-    return devmode;
+  DEVMODE devmode{};
+  devmode.dmSize = sizeof(devmode);
+  auto rval = EnumDisplaySettings(nullptr, ENUM_REGISTRY_SETTINGS, &devmode);
+  return devmode;
 }
 
 DWORD get_max_refresh_rate(uint16_t width, uint16_t height) {
-    DWORD max_refresh_rate = 0;
-    for (DWORD graphics_mode_index = 0;; ++graphics_mode_index) {
-        DEVMODE devmode{};
-        devmode.dmSize = sizeof(devmode);
-        auto rval = EnumDisplaySettings(0, graphics_mode_index, &devmode);
-        if (rval == 0)
-            break;
-        if (devmode.dmPelsWidth == width && devmode.dmPelsHeight == height)
-            max_refresh_rate = std::max(max_refresh_rate, devmode.dmDisplayFrequency);
-    }
-
-    return max_refresh_rate;
-}
-
-LONG _ChangeDisplaySettings(DEVMODE * devmode, DWORD dwFlags) {
-    DEVMODE current_settings{};
-    current_settings.dmSize = sizeof(current_settings);
-    EnumDisplaySettings(0, ENUM_CURRENT_SETTINGS, &current_settings);
-    if(current_settings.dmPelsWidth == devmode->dmPelsWidth && current_settings.dmPelsHeight == devmode->dmPelsHeight 
-            && ((devmode->dmFields & DM_DISPLAYFREQUENCY == 0) || devmode->dmDisplayFrequency == current_settings.dmDisplayFrequency))
-        return DISP_CHANGE_SUCCESSFUL;
-    
-    return ChangeDisplaySettings(devmode, dwFlags);
-    
-}
-
-
-template<typename log_function>
-void set_resolution(uint16_t width, uint16_t height, uint16_t refresh_rate, bool wait_on_process, bool refresh_rate_use_max, log_function log_func) {
+  DWORD max_refresh_rate = 0;
+  for (DWORD graphics_mode_index = 0;; ++graphics_mode_index) {
     DEVMODE devmode{};
     devmode.dmSize = sizeof(devmode);
-    devmode.dmPelsWidth = width;
-    devmode.dmPelsHeight = height;
-    devmode.dmFields = DM_PELSHEIGHT | DM_PELSWIDTH;
-    if (refresh_rate == 0 && refresh_rate_use_max) {
-        log_func("setting max refresh_rate"s);
-        refresh_rate = get_max_refresh_rate(width, height);
+    auto rval = EnumDisplaySettings(0, graphics_mode_index, &devmode);
+    if (rval == 0) {
+      break;
     }
-    else {
-        if (refresh_rate_use_max)
-            log_func("refresh_rate and refresh_rate_use_max specified, defaulting to specified rate"s);
+    if (devmode.dmPelsWidth == width && devmode.dmPelsHeight == height) {
+      max_refresh_rate = std::max(max_refresh_rate, devmode.dmDisplayFrequency);
     }
-    if (refresh_rate != 0) {
-        devmode.dmDisplayFrequency = refresh_rate;
-        devmode.dmFields |= DM_DISPLAYFREQUENCY;
-    }
-    DWORD result;
-    if(wait_on_process) // if we are waiting on the process we can clean up at the end
-        result = _ChangeDisplaySettings(&devmode, CDS_UPDATEREGISTRY);
-    else //otherwise we do the best we can
-        result = _ChangeDisplaySettings(&devmode, 0);
-    if (result != DISP_CHANGE_SUCCESSFUL) {
-        log_func("ChangeDisplaySettings failed error:" + std::to_string(result));
-    }
+  }
+
+  return max_refresh_rate;
 }
 
-void log(const std::string &message, std::ofstream &file, bool log_to_stdout = true)
-{
+LONG _ChangeDisplaySettings(DEVMODE *devmode, DWORD dwFlags) {
+  DEVMODE current_settings{};
+  current_settings.dmSize = sizeof(current_settings);
+  EnumDisplaySettings(0, ENUM_CURRENT_SETTINGS, &current_settings);
+  if (current_settings.dmPelsWidth == devmode->dmPelsWidth && current_settings.dmPelsHeight == devmode->dmPelsHeight &&
+      ((devmode->dmFields & DM_DISPLAYFREQUENCY == 0) || devmode->dmDisplayFrequency == current_settings.dmDisplayFrequency)) {
+    return DISP_CHANGE_SUCCESSFUL;
+  }
+
+  return ChangeDisplaySettings(devmode, dwFlags);
+}
+
+template <typename log_function>
+void set_resolution(uint16_t width, uint16_t height, uint16_t refresh_rate, bool wait_on_process, bool refresh_rate_use_max, log_function log_func) {
+  DEVMODE devmode{};
+  devmode.dmSize = sizeof(devmode);
+  devmode.dmPelsWidth = width;
+  devmode.dmPelsHeight = height;
+  devmode.dmFields = DM_PELSHEIGHT | DM_PELSWIDTH;
+  if (refresh_rate == 0 && refresh_rate_use_max) {
+    log_func("setting max refresh_rate"s);
+    refresh_rate = get_max_refresh_rate(width, height);
+  } else {
+    if (refresh_rate_use_max) {
+      log_func("refresh_rate and refresh_rate_use_max specified, defaulting to specified rate"s);
+    }
+  }
+  if (refresh_rate != 0) {
+    devmode.dmDisplayFrequency = refresh_rate;
+    devmode.dmFields |= DM_DISPLAYFREQUENCY;
+  }
+  DWORD result;
+  if (wait_on_process) {
+    // if we are waiting on the process we can clean up at the end
+    result = _ChangeDisplaySettings(&devmode, CDS_UPDATEREGISTRY);
+  } else {
+    // otherwise we do the best we can
+    result = _ChangeDisplaySettings(&devmode, 0);
+  }
+  if (result != DISP_CHANGE_SUCCESSFUL) {
+    log_func("ChangeDisplaySettings failed error:" + std::to_string(result));
+  }
+}
+
+void log(const std::string &message, std::ofstream &file, bool log_to_stdout = true) {
   auto now = std::chrono::system_clock::now();
   auto in_time_t = std::chrono::system_clock::to_time_t(now);
   auto time = std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
   file << time << ": " << message << std::endl;
-  if (log_to_stdout)
-  {
+  if (log_to_stdout) {
     std::cout << time << ": " << message << std::endl;
   }
   file.flush();
 }
 
-std::optional<fs::path> get_destination_folder_path()
-{
+std::optional<fs::path> get_destination_folder_path() {
   winreg::RegKey key;
   auto result = key.TryOpen(HKEY_CURRENT_USER, L"SOFTWARE\\lyckantropen\\moonlight_hdr_launcher");
-  if (result)
-  {
-    if (auto dest_path = key.TryGetStringValue(L"destination_folder"))
-    {
+  if (result) {
+    if (auto dest_path = key.TryGetStringValue(L"destination_folder")) {
       return fs::path(*dest_path);
-    }
-    else
-    {
+    } else {
       return {};
     }
-  }
-  else
-  {
+  } else {
     return {};
   }
 }
 
-LRESULT CALLBACK    WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message)
-    {
-    case WM_COMMAND:
-    {
-        int wmId = LOWORD(wParam);
-        // Parse the menu selections:
-        switch (wmId)
-        {
-        case 105: //IDM_EXIT
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
-        }
-    }
-    break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  switch (message) {
+  case WM_COMMAND: {
+    int wmId = LOWORD(wParam);
+    // Parse the menu selections:
+    switch (wmId) {
+    case 105: // IDM_EXIT
+      PostQuitMessage(0);
+      break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+      return DefWindowProc(hWnd, message, wParam, lParam);
     }
-    return 0;
+  } break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
 };
 
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
   auto argc = __argc;
   auto argv = __argv;
 
   auto pwd = fs::path(argv[0]).parent_path();
   auto reg_dest_path = get_destination_folder_path();
-  
-  if (reg_dest_path)
-  {
+
+  if (reg_dest_path) {
     pwd = *reg_dest_path;
   }
 
@@ -194,13 +181,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 
   log(std::string("Moonlight HDR Launcher Version ") + std::string(MHDRL_VERSION), logfile);
 
-  if (reg_dest_path)
-  {
+  if (reg_dest_path) {
     log(std::string("Working folder read from registry: ") + pwd.string(), logfile);
   }
 
-  for (int i = 0; i < argc; ++i)
-  {
+  for (int i = 0; i < argc; ++i) {
     log(std::string("argv[") + std::to_string(i) + std::string("]: ") + std::string(argv[i]), logfile);
   }
 
@@ -222,8 +207,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     bool disable_reset_display_mode = false;
     bool remote_desktop = false;
 
-    if (fs::exists(inifile))
-    {
+    if (fs::exists(inifile)) {
       log(std::string("Found config file: ") + inifile.string(), logfile);
 
       std::ifstream ini_f{inifile.c_str()};
@@ -240,11 +224,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
       disable_reset_display_mode = ini.get_optional<bool>("options.disable_reset_display_mode").get_value_or(false);
       remote_desktop = ini.get_optional<bool>("options.remote_desktop").get_value_or(false);
       if (launcher_exe != ""s) {
-          if (remote_desktop) log("remote_desktop and launcher_exe both specified defaulting to launcher_exe"s, logfile);
-          remote_desktop = false;
+        if (remote_desktop) {
+          log("remote_desktop and launcher_exe both specified defaulting to launcher_exe"s, logfile);
+        }
+        remote_desktop = false;
       }
-      if (!remote_desktop && launcher_exe == ""s)
-          launcher_exe = default_launcher;
+      if (!remote_desktop && launcher_exe == ""s) {
+        launcher_exe = default_launcher;
+      }
 
       log(std::string("options.launcher_exe=") + launcher_exe, logfile);
       log(std::string("options.wait_on_process=") + std::to_string(wait_on_process), logfile);
@@ -253,130 +240,108 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 #ifdef SENTRY_DEBUG
       sentry_value_t le_c = sentry_value_new_breadcrumb("default", "launcher_exe");
       sentry_value_set_by_key(le_c, "message", sentry_value_new_string(launcher_exe.c_str()));
-      sentry_add_breadcrumb(le_c);      
+      sentry_add_breadcrumb(le_c);
 #endif
     }
     DEVMODE original_display_mode{};
     if (resX != 0 && resY != 0) {
-        original_display_mode = get_primary_display_registry_settings();
-        set_resolution(resX, resY, refresh_rate, wait_on_process, refresh_rate_use_max, [&logfile](std::string a)->void {log(a, logfile); });
+      original_display_mode = get_primary_display_registry_settings();
+      set_resolution(resX, resY, refresh_rate, wait_on_process, refresh_rate_use_max, [&logfile](std::string a) -> void { log(a, logfile); });
     }
-    
-    if (wait_on_process)
-    {
+
+    if (wait_on_process) {
       WNDCLASS wc = {};
       wc.lpfnWndProc = WndProc;
       wc.hInstance = hInstance;
       wc.lpszClassName = L"DummyWindow";
       RegisterClass(&wc);
-      auto launcher_window = CreateWindowW(L"DummyWindow", L"MoonLight HDR Launcher Do Not Close", WS_OVERLAPPEDWINDOW,
-                                           0, 0, 1, 1, nullptr, nullptr, hInstance, nullptr);
-      if(launcher_window)
-          ShowWindow(launcher_window, nCmdShow);
+      auto launcher_window =
+          CreateWindowW(L"DummyWindow", L"MoonLight HDR Launcher Do Not Close", WS_OVERLAPPEDWINDOW, 0, 0, 1, 1, nullptr, nullptr, hInstance, nullptr);
+      if (launcher_window) {
+        ShowWindow(launcher_window, nCmdShow);
+      }
 
       std::optional<HdrToggle> hdr_toggle;
-      if (toggle_hdr)
-      {
+      if (toggle_hdr) {
         log("Attempting to set HDR mode", logfile);
 #ifndef SENTRY_DEBUG
         try
 #endif
         {
           hdr_toggle = HdrToggle{};
-          if (!hdr_toggle->set_hdr_mode(true))
-          {
+          if (!hdr_toggle->set_hdr_mode(true)) {
             log("Failed to set HDR mode", logfile);
           }
         }
 #ifndef SENTRY_DEBUG
-        catch (NvapiException &e)
-        {
+        catch (NvapiException &e) {
           log(std::string("Failed to set HDR mode: ") + e.what(), logfile);
         }
 #endif
       }
 
       if (!remote_desktop) {
-          log(std::string("Launching '") + launcher_exe + std::string("' and waiting for it to complete."), logfile);
-          bp::ipstream is; //reading pipe-stream
-          try
-          {
-              auto c = bp::child{ launcher_exe, bp::std_out > is, bp::std_err > is };
-              while (c.running())
-              {
-                  std::string line;
-                  if (std::getline(is, line) && !line.empty())
-                  {
-                      log(std::string("SUBPROCESS: ") + line, logfile);
-                  }
-              }
-              c.wait();
-              if (!disable_reset_display_mode && original_display_mode.dmSize > 0)
-                  _ChangeDisplaySettings(&original_display_mode, CDS_UPDATEREGISTRY);
-              if (c.exit_code() != 0)
-              {
-                  log(std::string("The command \"") + launcher_exe + std::string("\" has terminated with exit code ") + std::to_string(c.exit_code()), logfile);
-              }
+        log(std::string("Launching '") + launcher_exe + std::string("' and waiting for it to complete."), logfile);
+        bp::ipstream is; // reading pipe-stream
+        try {
+          auto c = bp::child{launcher_exe, bp::std_out > is, bp::std_err > is};
+          while (c.running()) {
+            std::string line;
+            if (std::getline(is, line) && !line.empty()) {
+              log(std::string("SUBPROCESS: ") + line, logfile);
+            }
           }
-          catch (bp::process_error const& e)
-          {
-              log(std::string("Error executing command. Error code: ") + std::to_string(e.code().value()) + ", message: " + e.code().message(), logfile);
+          c.wait();
+          if (!disable_reset_display_mode && original_display_mode.dmSize > 0)
+            _ChangeDisplaySettings(&original_display_mode, CDS_UPDATEREGISTRY);
+          if (c.exit_code() != 0) {
+            log(std::string("The command \"") + launcher_exe + std::string("\" has terminated with exit code ") + std::to_string(c.exit_code()), logfile);
+          }
+        } catch (bp::process_error const &e) {
+          log(std::string("Error executing command. Error code: ") + std::to_string(e.code().value()) + ", message: " + e.code().message(), logfile);
 #ifdef SENTRY_DEBUG
-              sentry_value_t exc = sentry_value_new_object();
-              sentry_value_set_by_key(exc, "type", sentry_value_new_string("bp::process_error"));
-              sentry_value_set_by_key(exc, "code", sentry_value_new_int32(e.code().value()));
+          sentry_value_t exc = sentry_value_new_object();
+          sentry_value_set_by_key(exc, "type", sentry_value_new_string("bp::process_error"));
+          sentry_value_set_by_key(exc, "code", sentry_value_new_int32(e.code().value()));
 
-              sentry_value_t event = sentry_value_new_event();
-              sentry_value_set_by_key(event, "exception", exc);
-              sentry_capture_event(event);
+          sentry_value_t event = sentry_value_new_event();
+          sentry_value_set_by_key(event, "exception", exc);
+          sentry_capture_event(event);
 #endif
-          }
-      }
-      else {
-          MSG msg;
-          while (GetMessage(&msg, launcher_window, 0, 0))
-          {
-              if (msg.hwnd == 0)
-                  break;
-              DispatchMessage(&msg);
-          }
-
-      }
-
-      if (toggle_hdr)
-      {
-        log("Attempting to disable HDR mode", logfile);
-        try
-        {
-          hdr_toggle->set_hdr_mode(false);
         }
-        catch (NvapiException &e)
-        {
+      } else {
+        MSG msg;
+        while (GetMessage(&msg, launcher_window, 0, 0)) {
+          if (msg.hwnd == 0) {
+            break;
+          }
+          DispatchMessage(&msg);
+        }
+      }
+
+      if (toggle_hdr) {
+        log("Attempting to disable HDR mode", logfile);
+        try {
+          hdr_toggle->set_hdr_mode(false);
+        } catch (NvapiException &e) {
           log(std::string("Failed to disable HDR mode: ") + e.what(), logfile);
         }
       }
-      if(launcher_window)
+      if (launcher_window) {
         DestroyWindow(launcher_window);
-
-    }
-    else
-    {
+      }
+    } else {
       log(std::string("Launching '") + launcher_exe + std::string("' and detaching immediately."), logfile);
       bp::spawn(launcher_exe);
     }
     retcode = 0;
   }
 #ifndef SENTRY_DEBUG
-  catch (std::runtime_error &e)
-  {
+  catch (std::runtime_error &e) {
     log(std::string("Error: ") + std::string(e.what()), logfile);
-  }
-  catch (std::exception &e)
-  {
+  } catch (std::exception &e) {
     log(std::string("Error: ") + std::string(e.what()), logfile);
-  }
-  catch (...)
-  {
+  } catch (...) {
     log("Unknown error", logfile);
   }
 #endif
@@ -384,8 +349,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 #ifdef SENTRY_DEBUG
   sentry_shutdown();
 #endif
-
-  
 
   return retcode;
 }
